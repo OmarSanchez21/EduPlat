e89dacc2b4e5b21af2f3799a3643fbd83dcfde7b
Omar Sanchez <omisp21@gmail.com>
2024-04-27 19:02:21 -0400

    Revert "creacion basica del endpoint de SaveUsers"
    This reverts commit f15f37e43255cce20018411d467509b8085f9cde.

--- server/routes/User.js
+++
@@ -1,9 +0,0 @@
-// routes/userRoutes.js
-const express = require('express');
-const router = express.Router();
-const { createUser, getUserById } = require('../controllers/User');
-
-router.post('/', createUser);
-
-
-module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,34 +1,13 @@
-// Importar las dependencias necesarias
 const express = require('express');
-const mongoose = require('mongoose');
-const userRoutes = require('./routes/User'); // Importar las rutas relacionadas con los usuarios
 
-// Crear una instancia de la aplicación Express
 const app = express();
 
-// Configurar middleware para procesar el cuerpo de las solicitudes en formato JSON
-app.use(express.json());
-
-// Configurar las rutas de la aplicación
-app.use('/users', userRoutes);
-
-//el home de express
-app.get('/',(req,res) => {
-    res.send("Hola desde express")
-})
-// Configurar la conexión a la base de datos MongoDB Atlas
-const mongoURI = 'mongodb+srv://rooter:uxncSfbKzmPn9s58@bdsocialnex.uagax6k.mongodb.net/<databaseName>?retryWrites=true&w=majority&appName=BDSocialNex';
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('Connected to MongoDB Atlas');
-  })
-  .catch((error) => {
-    console.error('Error connecting to MongoDB Atlas:', error);
-  });
-
-// Configurar el puerto en el que la aplicación escuchará las solicitudes
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log(`Server is running on port ${PORT}`);
-  console.log('\x1b[36m',`http://localhost:${PORT}`);
+app.get('/',(req, res) =>{
+    res.send("Hola desde Express");
 });
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>{
+    console.log(`Servidor Express escuchando en el puerto: ${PORT}`);
+    console.log(`http://localhost:${PORT}`)
+})
