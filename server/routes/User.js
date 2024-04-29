4b69c71e59b0496a2444fc99ffd90716e53f157d
OmarSanchez21 <125134492+OmarSanchez21@users.noreply.github.com>
2024-04-29 10:16:25 -0400

    Revert "Janiel f"

--- server/routes/User.js
+++
@@ -1,9 +0,0 @@
-// routes/userRoutes.js
-const express = require('express');
-const router = express.Router();
-const { createUser, getallUsers } = require('../controllers/User');
-const UserControllers = require('../controllers/User');
-router.post('/', createUser);
-router.get('/', getallUsers);
-
-module.exports = router;
--- server/server.js
+++ server/server.js
@@ -1,31 +1,13 @@
-// Importar las dependencias necesarias
 const express = require('express');
-const mongoose = require('mongoose');
-const userRoutes = require('./routes/User'); // Importar las rutas relacionadas con los usuarios
 
-// Crear una instancia de la aplicación Express
 const app = express();
 
-// Configurar middleware para procesar el cuerpo de las solicitudes en formato JSON
-app.use(express.json());
-
-// Configurar las rutas de la aplicación
-app.use('/users', userRoutes); // Usar las rutas relacionadas con los usuarios en la ruta '/users'
-
-// Configurar la conexión a la base de datos MongoDB Atlas
-const mongoURI = 'mongodb+srv://rooter:uxncSfbKzmPn9s58@bdsocialnex.uagax6k.mongodb.net/<databaseName>?retryWrites=true&w=majority&appName=BDSocialNex';
-mongoose.connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
-  .then(() => {
-    console.log('\x1b[32m', 'Connected to MongoDB Atlas'); // Texto verde
-  })
-  .catch((error) => {
-    console.error('\x1b[31m', 'Error connecting to MongoDB Atlas:', error); // Texto rojo
-  });
-
-// Configurar el puerto en el que la aplicación escuchará las solicitudes
-// Configurar el puerto en el que la aplicación escuchará las solicitudes
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-  console.log('\x1b[36m', `Server is running on http://localhost:${PORT}`); // Texto cyan
+app.get('/',(req, res) =>{
+    res.send("Hola desde Express");
 });
 
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () =>{
+    console.log(`Servidor Express escuchando en el puerto: ${PORT}`);
+    console.log(`http://localhost:${PORT}`)
+})
